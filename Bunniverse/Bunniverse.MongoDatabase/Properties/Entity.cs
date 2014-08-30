cf4975f3283b53d8d1bff29e723441c4794b0807
Viktor <viktordakov97@gmail.com>
2014-08-30 14:18:42 +0300

    Created new project MongoDatabase. Created a working code first local mongodb.

---
+++ Bunniverse/Bunniverse.MongoDatabase/Properties/Entity.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunniverse
+{
+    public class Entity
+    {
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
--- Bunniverse/Bunniverse/BunniverseEntryPoint.cs
+++ Bunniverse/Bunniverse/BunniverseEntryPoint.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bunniverse
+﻿namespace Bunniverse
 {
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using MongoDB.Driver.Builders;
+    using MongoDB.Driver.GridFS;
+    using MongoDB.Driver.Linq;
+    using System;
+
     class BunniverseEntryPoint
     {
         static void Main(string[] args)
